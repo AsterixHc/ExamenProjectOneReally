8e5f7d59394e61da34ccb22c9daa55aba2948d17
Daniel Justesen <Daniel@justesen.biz>
2019-11-29 15:03:53 +0100

    stuff
    added all the stuff

---
+++ ExamenProjectReal/ExamenProjectReal/Item/PhoenixDown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProjectReal
+{
+    class PhoenixDown : Item
+    {
+        //If Player is dead
+        //Revive Player at 40% Health
+    }
+}
